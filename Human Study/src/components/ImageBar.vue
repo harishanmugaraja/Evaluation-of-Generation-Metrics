2506b92364874bf9355196b01ed0839136c188ec
harishanmugaraja <harishanmugaraja@gmail.com>
2022-10-16 01:42:40 -0400

    first commit, added human study framework

---
+++ Human Study/src/components/ImageBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class = 'rectangle'>
+        <img v-for="image in this.imgarr" :src="image" />
+    </div>
+</template>
+
+<style scoped>
+img {
+    width: 100px;
+    height: 100px;
+    padding: 1px;
+}
+.rectangle {
+width:520px;
+height:105px;
+border:1px solid #000;
+}
+</style>
+
+<script>
+export default {
+    props: {
+    imgarr: Array,
+  },
+
+  created() {
+    console.log(this.imgarr);
+  },
+
+}
+</script>
+
---
+++ Human Study/src/components/SortGroups.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- <draggable v-model="imagebars">
+        <ImageBar v-for="imagebar in imagebars" :key="imagebar.id" :imgarr="imagebar"></ImageBar>
+        <div :id="element.id" v-for="element in imagebars" :key="element.id">
+          <strong>{{element.name}}</strong>
+          <span>{{element.images[0]}}</span>
+        </div>
+    </draggable> -->
+ <ImageBar v-for="images in imagebars" :imgarr="images.images"></ImageBar>
+ <!-- <ImageBar :imgarr="['../assets/images/group2/p1.png','../assets/images/group2/p2.png','../assets/images/group2/p3.png','../assets/images/group2/p4.png']"></ImageBar> -->
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import ImageBar from './ImageBar.vue';
+import draggable from 'vuedraggable';
+
+export default {
+    name: 'sort-groups',
+
+    components: {
+        'ImageBar': ImageBar,
+        'draggable': draggable
+    },
+
+    data() {
+        return {
+            imagebars: [
+                {id: 0, images: ['../assets/images/group1/p1.png','../assets/images/group1/p2.png','../assets/images/group1/p3.png','../assets/images/group1/p4.png']},
+                {id: 1, images: ['../assets/images/group2/p1.png','../assets/images/group2/p2.png','../assets/images/group2/p3.png','../assets/images/group2/p4.png']}
+            ],
+        };
+    },
+
+}
+
+</script>
