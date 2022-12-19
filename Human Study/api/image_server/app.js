840af1132bfd7b7f0f8e9ebd3ae7475ab0cf31ce
Sathvik Ravi <sathvikravi@Sathviks-MacBook-Air.local>
2022-12-19 16:13:51 -0500

    Added submit checking functionality

--- Human Study/api/image_server/app.js
+++ Human Study/api/image_server/app.js
@@ -2,11 +2,27 @@
    Very little programming is required as express can easily be configured to be a static image server.
 */
 const express = require('express')
+const bp = require('body-parser')
+const cors = require('cors');
 const app = express()
 const port = 3030
 
+app.use(cors())
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/submit-form', (req, res) => {
+  // req.body contains the form data
+  console.log(req.body)
+
+  // do something with the form data, like save it to a database or send it via email
+
+  // send a response to the client
+  res.send('Form submitted successfully!')
 })
 
 const path = require('path');
--- Human Study/src/App.vue
+++ Human Study/src/App.vue
@@ -1,3 +1,53 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'all-groups',
+  data() {
+    return {
+      formData: {
+        // form field values go here
+        user_ranking: [],
+        user_q2_bin1_elements: [],
+        user_q2_bin2_elements: [],
+        user_q3_group_selected: -1,
+        user_q4_image_selected: null,
+        user_q5_bin1_elements: [],
+        user_q5_bin2_elements: []
+      }
+    }
+  },
+  methods: {
+    async submitForm(e) {
+      if (this.$refs['bin-groups-1'].bin1.length == 4 && this.$refs['bin-groups-1'].bin2.length == 4 && this.$refs['image-grid-whole'].selectNum > -1 && this.$refs['image-grid-single'].selectNum > -1 && this.$refs['bin-groups-2'].bin1.length == 4 && this.$refs['bin-groups-2'].bin2.length == 4) {
+        this.formData.user_ranking = JSON.parse(JSON.stringify(this.$refs['rank-groups'].imagebars))
+        this.formData.user_q2_bin1_elements = JSON.parse(JSON.stringify(this.$refs['bin-groups-1'].bin1))
+        this.formData.user_q2_bin2_elements = JSON.parse(JSON.stringify(this.$refs['bin-groups-1'].bin2))
+        this.formData.user_q3_group_selected = this.$refs['image-grid-whole'].selectNum
+        this.formData.user_q4_image_selected = this.$refs['image-grid-single'].selectNum
+        this.formData.user_q5_bin1_elements = JSON.parse(JSON.stringify(this.$refs['bin-groups-2'].bin1))
+        this.formData.user_q5_bin2_elements = JSON.parse(JSON.stringify(this.$refs['bin-groups-2'].bin2))
+        // this.formData = JSON.parse(JSON.stringify(this.formData))
+        console.log(JSON.parse(JSON.stringify(this.formData)))
+        console.log("true is the value")
+        e.preventDefault()
+        try {
+          const response = await axios.post('http://localhost:3030/submit-form', JSON.parse(JSON.stringify(this.formData)))
+          console.log(response)
+          // handle successful form submission
+        } catch (error) {
+          console.log(error)
+          // handle failed form submission
+        }
+        return true;
+      }
+      e.preventDefault()
+      console.log("false is the value")
+      return false;
+    }
+  }
+}
+</script>
 <script setup>
 import RankGroups from './components/RankGroups.vue'
 import BinGroups from './components/BinGroups.vue'
@@ -8,13 +58,17 @@ import ImageGridWhole from './components/ImageGridWhole.vue';
 <!-- This is the file where the components get mounted to as a final HTML file -->
 
 <template>
-  <div class="app">
-    <RankGroups :prompt = "'Rank these groups of images from highest image quality to the lowest'"></RankGroups>
-    <BinGroups :prompt = "'These images belong to one of two different groups, seperate them into their respective groups'"></BinGroups>
-    <ImageGridWhole :prompt = "'Select the group of images below that has the highest image quality'"></ImageGridWhole>
-    <ImageGridSingle :prompt = "'Select the image from the group of images below that does not belong'"></ImageGridSingle>
-    <BinGroups :prompt = "'These images belong to one of two different groups, seperate them into their respective groups'"></BinGroups>
-  </div>
+  <form method="post" action="/submit-form" @submit="submitForm">
+    <div class="app">
+      <RankGroups ref="rank-groups" :prompt = "'Rank these groups of images from highest image quality to the lowest'"></RankGroups>
+      <BinGroups ref="bin-groups-1" :prompt = "'These images belong to one of two different groups, seperate them into their respective groups'"></BinGroups>
+      <ImageGridWhole ref="image-grid-whole" :prompt = "'Select the group of images below that has the highest image quality'"></ImageGridWhole>
+      <ImageGridSingle ref="image-grid-single" :prompt = "'Select the image from the group of images below that does not belong'"></ImageGridSingle>
+      <BinGroups ref="bin-groups-2" :prompt = "'These images belong to one of two different groups, seperate them into their respective groups'"></BinGroups>
+      <button type="submit">Submit</button>
+      <!-- <button>Emit Event</button> -->
+    </div>
+  </form>
 </template>
 
 <style scoped>
